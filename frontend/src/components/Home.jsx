22aeeb51157e6f77b8aa97601b2ae8c2af660439
bhanu yadav <bhyadav.2002@gmail.com>
2024-08-02 15:27:23 +0530

    login/signup page created

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      {/* <Navbar/> */}
+      HOME
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/src/components/auth/Login.jsx
@@ -0,0 +1,105 @@
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Label } from "../ui/label";
+import {RadioGroup, RadioGroupItem } from "../ui/radio-group";
+import { Link, useNavigate} from "react-router-dom";
+import { useState } from "react";
+import { toast } from "sonner";
+import axios from "axios"
+import { USER_API_END_POINT } from "@/utils/constant";
+
+
+const Login = () => {
+    const [input, setInput] = useState({
+        fullname:"",
+        email:"",
+        phoneNumber:"",
+        password:"",
+        role:"",
+        file:""
+    });
+
+    const navigate = useNavigate();
+
+    const changeEventHandler = (e) =>{
+        setInput({...input, [e.target.name]:e.target.value});
+    }
+
+    const changeFileHandler = () =>{
+        setInput({...input, file:e.target.files?.[0]});
+
+    }
+
+    const submitHandler = async (e) =>{
+        e.preventDefault();
+        try{
+            const res = await axios.post(`${USER_API_END_POINT}/login`,input, {
+                headers:{
+                    "Content-Type":"application/json"
+                },
+                withCredentials:true,
+            })
+            if(res.data.success){
+                navigate("/")
+                toast.success(res.data.message);
+            }
+        }catch(err){
+            console.log(err)
+            toast.error(err.response.data.message)
+        }
+    }
+  return (
+    <>
+      {/* <Navbar/> */}
+      <div className="flex items-center justify-center max-w-7xl mx-auto">
+        <form
+          onSubmit={submitHandler}
+          className="w-1/2 border border-gray-200 rounded-md p-4 my-10 "
+        >
+          <h1 className="font-bold text-xl mb-5">Login</h1>
+          <div className="my-2">
+            <Label>Email</Label>
+            <Input type="email" value={input.email} name="email" onChange={changeEventHandler} placeholder="email@gmail.com" />
+          </div>
+          <div className="my-2">
+            <Label>Password</Label>
+            <Input type="password" value={input.password} name="password" onChange={changeEventHandler} placeholder="Password" />
+          </div>
+          <div className="flex items-center justify-between">
+            <RadioGroup className="flex items-center gap-4 my-5">
+              <div className="flex items-center space-x-2">
+                <Input
+                type="radio"
+                name="role"
+                value="student"
+                checked = {input.role === "student"}
+                onChange={changeEventHandler}
+                className="cursor-pointer"
+                />
+                <Label htmlFor="option-one">Student</Label>
+              </div>
+              <div className="flex items-center space-x-2">
+              <Input
+                type="radio"
+                name="role"
+                value="recruiter"
+                checked = {input.role === "recruiter"}
+                onChange={changeEventHandler}
+                className="cursor-pointer"
+                />
+                <Label htmlFor="option-two">Recruiter</Label>
+              </div>
+            </RadioGroup>
+          </div>
+          <Button
+          type="submit"
+          className="w-full my-4"
+          >Login</Button>
+          <span className="text-sm">Don't have an account? <Link to="/signup" className="text-blue-600">Signup</Link> </span>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Login;
