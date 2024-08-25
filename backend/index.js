3f77cc5fe1bed7ed331b19bcf54388c3c964d0e6
Shivansh Khokhar <shivakhokhar4600@gmail.com>
2024-08-25 14:05:55 +0530

    added vercel files and ready for deployment

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-// import bodyParser from "body-parser";
 import connectDB from "./utils/db.js";
 import companyRoute from "./routes/company.route.js";
 import userRoute from "./routes/user.route.js";
@@ -20,11 +19,12 @@ app.get("/home",(req,res)=>{
 })
 
 // middlewares
+const clientUrl = process.env.CLIENT_URL;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 const corsOption = {
-    origin:'http://localhost:5173',
+    origin:clientUrl,
     credentials:true
 }
 app.use(cors(corsOption));
