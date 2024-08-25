9477ce90f4c8e51af5037ff33f78c7d66bc76555
Shivansh Khokhar <shivakhokhar4600@gmail.com>
2024-08-25 15:44:12 +0530

    changing Cors origin in index.js

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 const corsOption = {
-    origin:clientUrl,
+    origin:`${clientUrl}`,
     credentials:true
 }
 app.use(cors(corsOption));
