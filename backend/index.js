c8bc17e495064d8f5447d1c58e7b7d2edc12c1e7
Shivansh Khokhar <91069621+shivansh-khokhar@users.noreply.github.com>
2024-08-26 13:51:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOption = {
-  origin: `${clientUrl}`,
+  origin: `https://job-portal-awej.vercel.app`,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   optionsSuccessStatus: 200, // Some legacy browsers choke on 204
