4e4c377abd023d3fc88417ec5fcf5ab2c8eb5d9a
Shivansh Khokhar <shivakhokhar4600@gmail.com>
2024-08-28 01:30:01 +0530

    changed cloudinary.js

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 cloudinary.config({
     cloud_name:process.env.CLOUD_NAME,
-    api_key:process.env.api_key,
+    api_key:process.env.API_KEY,
     api_secret:process.env.API_SECRET
 });
 
